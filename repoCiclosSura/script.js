6322b6114d9e5e46136148fb50be96ce2f4c35ef
Natalia Giraldo <nataliagigo@hotmail.es>
2024-09-23 11:58:24 -0500

    ciclos en javascript

---
+++ repoCiclosSura/script.js
@@ -0,0 +1,54 @@
+// let acumuladoSalario = 0;
+// for (let index = 1; index <=1 10; index++) {
+//   let nombreEmpleado = prompt('Ingrese el nombre del empleado: ')
+//   let salarioEmpleado = Number(prompt('Ingrese el salario: '))
+//   acumuladoSalario = acumuladoSalario + salarioEmpleado
+//   console.log('El promedio salarial es: ' + (acumuladoSalario / index))
+// }
+
+// let acumuladoSalario = 0;
+// for (var index = 1; index <= 10; index++) {
+//     if (index % 2 == 0){
+//         let nombreEmpleado = prompt('Ingrese el nombre del empleado: ')
+//         let salarioEmpleado = Number(prompt('Ingrese el salario: '))
+//         acumuladoSalario = acumuladoSalario + salarioEmpleado
+
+//     }
+// }
+// console.log('El promedio salarial es: ' + (acumuladoSalario / index))
+
+// let resultado = 0;
+// for (var index = 1; index <= 10; index++) {
+
+//         let resultado = console.log(" 5 x " + index + " : " + (5*index))
+
+//     }
+
+// let numero = 1
+// while (numero <= 10) {
+//     console.log(" 5 x " + numero + " : " + (5*numero))
+//   numero++
+// }
+
+
+// let numero = 1
+// do{
+//     console.log('5 x ' + numero + ' = ' + (numero * 5))
+//     numero++
+// }while(numero <= 10)
+
+// let repetir = true
+// let acumuladoSalario = 0
+// let contadorSalarios = 0
+// while (repetir){
+//     let nombreEmpleado = prompt('Ingrese el nombre del empleado: ')
+//     let salarioEmpleado = Number(prompt('Ingrese el salario: '))
+//     acumuladoSalario = acumuladoSalario + salarioEmpleado
+//     contadorSalarios++
+//     let opcion = prompt('¿Desea repetir el código? \n1 - si \n2 - No')
+//     if (opcion ==  '2'){
+//         repetir = false
+//     }
+// }
+// console.log(contadorSalarios)
+// console.log(acumuladoSalario / contadorSalarios)
